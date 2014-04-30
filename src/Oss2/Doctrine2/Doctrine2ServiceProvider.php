63218e74c8e9868606f6adc0b30472f47a5662e9
Roland Huszti <roland@opensolutions.ie>
2014-04-30 11:45:28 +0100

    [HK] Tabs -> spaces

--- src/Oss2/Doctrine2/Doctrine2ServiceProvider.php
+++ src/Oss2/Doctrine2/Doctrine2ServiceProvider.php
@@ -4,31 +4,32 @@ use Illuminate\Support\ServiceProvider;
 
 class Doctrine2ServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
